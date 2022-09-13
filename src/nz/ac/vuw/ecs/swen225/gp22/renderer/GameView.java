cd9cf6f6fc7a3613b1e381122c9a3e4225e5f32d
grunfejack <grunfejack@myvuws.ac.nz>
2022-09-13 14:59:09 +1200

    image class with enums to change img files

---
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/GameView.java
@@ -0,0 +1,12 @@
+
+package nz.ac.vuw.ecs.swen225.gp22.renderer;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
+class GameView extedns JPanel{
+    
+}
