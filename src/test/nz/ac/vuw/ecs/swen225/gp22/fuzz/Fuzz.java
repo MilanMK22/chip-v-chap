b4b89ce7921688e9af61669bd749a72b7681ff55
mashkoilya <mashkoilya@ecs.vuw.ac.nz>
2022-09-25 15:29:16 +1300

    almost working fuzz. just need a game. also commented out the asserts in keys.java

--- src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/Fuzz.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/Fuzz.java
@@ -13,45 +13,56 @@ import org.junit.Assert.*;
 
 import java.awt.Component;
 import java.awt.event.KeyEvent;
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.Canvas;
 
-class Fuzz{
+public class Fuzz{
     @Test
     public void test1(){
-        int[] array = {0,1,2,3};
+        Robot robot = null;
+        try{robot = new Robot();}
+        catch(AWTException e){e.printStackTrace();}
         
-        
-        
+        //int[] array = {KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT};
+        int[] array = {0, 1, 2, 3};
         Level level = Level.level1(null, null, array);
-        
         List<String> possibleInput = List.of("up", "down", "left", "right");
-        String randomInput = "";
         
-        for(int x = 0; x < 4; x++){
-            var randomNumber = (int)Math.random()*4;
-            randomInput = possibleInput.get(randomNumber);
-            
-            switch(randomInput){
+        long startTime = System.currentTimeMillis();
+
+        while(true){
+            switch(possibleInput.get((int)Math.random()*4)){
                 case "up":
-                    KeyEvent upkey = new KeyEvent(null, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,  0,'Z');
+                    // robot.keyPress(KeyEvent.VK_UP);
+                    // robot.keyRelease(KeyEvent.VK_UP);
+                    KeyEvent upkey = new KeyEvent(new Canvas(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,  0,'Z');
                     level.c().keyPressed(upkey);
                     level.c().keyReleased(upkey);
                     break;
                 case "down":
-                    KeyEvent downkey = new KeyEvent(null, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,  1,'Z');
+                    // robot.keyPress(KeyEvent.VK_DOWN);
+                    // robot.keyRelease(KeyEvent.VK_DOWN);
+                    KeyEvent downkey = new KeyEvent(new Canvas(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,  1,'Z');
                     level.c().keyPressed(downkey);
                     level.c().keyReleased(downkey);
                     break;
                 case "left":
-                    KeyEvent leftkey = new KeyEvent(null, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,  2,'Z');
+                    // robot.keyPress(KeyEvent.VK_LEFT);
+                    // robot.keyRelease(KeyEvent.VK_LEFT);
+                    KeyEvent leftkey = new KeyEvent(new Canvas(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,  2,'Z');
                     level.c().keyPressed(leftkey);
                     level.c().keyReleased(leftkey);
                     break;
                 case "right":
-                    KeyEvent rightkey = new KeyEvent(null, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,  3,'Z');
+                    // robot.keyPress(KeyEvent.VK_RIGHT);
+                    // robot.keyRelease(KeyEvent.VK_RIGHT);
+                    KeyEvent rightkey = new KeyEvent(new Canvas(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0,  3,'Z');
                     level.c().keyPressed(rightkey);
                     level.c().keyReleased(rightkey);
                     break;
             }
+            if(System.currentTimeMillis() >= startTime + 60000) return;
         }
     }
     
