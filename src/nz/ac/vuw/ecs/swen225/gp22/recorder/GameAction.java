6c43fc62f851c987308234c35c0155a257d04c34
Milan Kriletich <kriletmila@ecs.vuw.ac.nz>
2022-09-29 12:55:28 +1300

    Changing Action class to GameAction

--- src/nz/ac/vuw/ecs/swen225/gp22/recorder/GameAction.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/GameAction.java
@@ -1,12 +1,12 @@
 package nz.ac.vuw.ecs.swen225.gp22.recorder;
 
-public class Action {
+public class GameAction {
 
     String name;   // name of action
     int numPings;  //number of pings since last action 
 
 
-    Action(String name, int pings){
+    GameAction(String name, int pings){
 
         this.name = name;
         this.numPings = pings;
