7158921fe3c38ac7f1400ee09342823437cc8426
Milan Kriletich <kriletmila@ecs.vuw.ac.nz>
2022-10-03 13:54:42 +1300

    Using a Queue and adding time moves happened

--- src/nz/ac/vuw/ecs/swen225/gp22/recorder/CreatRepXML.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/CreatRepXML.java
@@ -1,6 +1,7 @@
 package nz.ac.vuw.ecs.swen225.gp22.recorder;
 
-import java.util.Stack;
+import java.util.LinkedList;
+
 
 // Just a test to make sure a replay can be turned into an XML
 public class CreatRepXML {
@@ -9,9 +10,12 @@ public class CreatRepXML {
 
     public static void main(String[] args) throws Exception {
         
-        Replay R2 = new Replay(new Stack<GameAction>(),1, "");
+        Replay R2 = new Replay(new LinkedList<GameAction>(),1, "");
         R2.addMove(new GameAction("UP", 30));
-        R2.addMove(new GameAction("UP", 30));
+        R2.addMove(new GameAction("Down", 30));
+        R2.addMove(new GameAction("Down", 30));
+        R2.addMove(new GameAction("Down", 30));
+
         R2.saveReplay();
 
 }
