c84d849ada0b272787e82693f5cf6741ba1cf3d1
Leo Gaynor <gaynorleo@ecs.vuw.ac.nz>
2022-10-03 17:00:57 +1300

    Monster Class

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/Monster.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/Monster.java
@@ -16,8 +16,8 @@ public class Monster implements Entity{
     @Override
     public Point getLocation() { return location; }
     public void setState(MonsterState s){ this.state = s; }
-    private void setLocation(Point p){ this.location = p; }
-    public void tick(){ state.tick(this); }
+    public void setLocation(Point p){ this.location = p; }
+    public void tick(Maze m){ state.tick(m, this); }
     public BufferedImage getImage(){ return Img.Marco.image; }
 }
 
@@ -25,21 +25,51 @@ public class Monster implements Entity{
 
 
 interface MonsterState{
-    public void tick(Monster m);
+    public void tick(Maze maze, Monster m);
 }
 
 class MappedMonster implements MonsterState{
-    
 
+    char[] moves;
+    int tickcount;
+    int cycle;
 
     MappedMonster(String mapping){
-        
+        moves = mapping.toCharArray();
+        tickcount = 0;
+        cycle = 0;
     }
-
 
     @Override
-    public void tick(Monster m){
-
+    public void tick(Maze maze, Monster m){
+        tickcount ++;
+        if(tickcount >= 20){
+            tickcount = 0;
+            if(cycle >= moves.length){
+                cycle = 0;
+            }
+            move(moveFromChar(moves[cycle], m), maze, m);
+            cycle ++;
+        }
     }
 
+    
+    private Point moveFromChar(char move, Monster m){
+        switch (move){
+            case 'u': return m.getLocation().up();
+            case 'd': return m.getLocation().down();
+            case 'l': return m.getLocation().left();
+            case 'r': return m.getLocation().right();
+            default: return m.getLocation();
+        }
+    }
+
+    private void move(Point p, Maze maze, Monster m){
+        if(maze.getTile(p).isFree() && !maze.getTile(p).hasPickup()){
+            maze.getTile(m.getLocation()).removeEntity();
+            maze.getTile(p).setEntity(m);
+            m.setLocation(p);
+        }
+        else throw new IllegalArgumentException("Monster "+ m+ " is trying to move to an illegal tile");
+    }
 }
