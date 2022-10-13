46b88f8112288489a669d3afc051e3e3b956cde9
grunfejack <grunfejack@ecs.vuw.ac.nz>
2022-10-13 19:44:59 +1300

    fixing sound

--- src/sounds/sounds.java
+++ src/sounds/sounds.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
-import java.util.Currency;
 
 import javax.print.DocFlavor.URL;
 import javax.sound.sampled.AudioInputStream;
@@ -46,14 +45,34 @@ public class sounds {
                 clip.stop();
             }
         }
-        public void loop(){
-            Clip cur = clip;
-            cur.loop(Clip.LOOP_CONTINUOUSLY);
+        public void looping(){
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
         }
     }
+
+
+
+
+    public Clip clip;
+    AudioInputStream sound;
+    public void setFile(String soundFileName) {
+   ß     try {
+            File file = new File(soundFileName);
+            AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+            clip = AudioSystem.getClip();
+            clip.open(sound);
+        } catch (Exception e) {
+        }
+    }
+    public void play() {
+        clip.start();
+    }
+    public void stop(){
+        clip.close();
+        clip.stop();
+    }
+
+    public void loop(){
+       // clip.loop(Clip.LOOP_CONTINUSESLY);
+    }
 }
-
-
-
-
-   
