d86d95477d30b23964105d947ba3392e6eb3b914
Calvin Li <callvinlinz@gmail.com>
2022-10-13 23:54:35 +1300

    finished javadocs

--- src/nz/ac/vuw/ecs/swen225/gp22/app/Board.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Board.java
@@ -10,23 +10,50 @@ import javax.swing.SwingConstants;
 import java.awt.*;
 
 
+/**
+ * Board Helper Class to get labels.
+ */
 
 public class Board extends JFrame {
+
+    /**
+     * Gets Level label
+     * @param levelNum
+     * @return level
+     */
     public static JLabel getLevelLabel(int levelNum){
         var level = new JLabel("LEVEL "+ levelNum,SwingConstants.CENTER);
         level.setBounds(613, 77, 60, 30);
         return level;
     }
+
+    /**
+     * Gets Chip Label
+     * @param levelNum
+     * @return chips
+     */
     public static JLabel getChipLabel(int numOfChips){
         var chips = new JLabel(Integer.toString(numOfChips),SwingConstants.CENTER);
         chips.setBounds(615, 203,60, 30);
         return chips;
     }
+
+    /**
+     * Gets Inventory Label
+     * @param levelNum
+     * @return inventory
+     */
     public static JLabel getInventory(){
         var inventory = new JLabel();
         inventory.setBounds(585,150,119,53);
         return inventory;
     }
+
+    /**
+     * Gets Background Image
+     * @param levelNum
+     * @return backgroundImage
+     */
     public static JLabel getBackgroundImage(){
         var backgroundImage = new JLabel();
         backgroundImage.setBounds(0,0,800,425);
@@ -34,6 +61,12 @@ public class Board extends JFrame {
         backgroundImage.setIcon(img);
         return backgroundImage;
     }
+
+    /**
+     * Gets Pause dialog
+     * @param levelNum
+     * @return dialog
+     */
     public static JDialog getPause(){
         JOptionPane pane = new JOptionPane("Paused", JOptionPane.INFORMATION_MESSAGE);
         JDialog dialog = pane.createDialog(null, "Paused");
@@ -42,6 +75,12 @@ public class Board extends JFrame {
         dialog.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         return dialog;
     }
+
+    /**
+     * Gets Timeout Dialog
+     * @param levelNum
+     * @return dialog
+     */
     public static JDialog getTimeout(){
         JOptionPane pane = new JOptionPane("Time Out", JOptionPane.INFORMATION_MESSAGE);
         JDialog dialog = pane.createDialog(null, "Time Out");
@@ -50,6 +89,12 @@ public class Board extends JFrame {
         dialog.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         return dialog;
     }
+    /**
+     * Gets Info Label
+     * @param levelNum
+     * @return info
+     */
+
     public static JLabel getInfo(){
         JLabel info = new JLabel();
         info.setBounds(55, 350,400, 60);
@@ -61,6 +106,12 @@ public class Board extends JFrame {
         return info;
     }
 
+
+    /**
+     * Gets InfoText
+     * @param levelNum
+     * @return infoText
+     */
     public static JLabel getInfoText(String infoString){
         JLabel infoText = new JLabel(infoString,SwingConstants.CENTER);
         infoText.setBounds(0,20,400,20);
--- src/nz/ac/vuw/ecs/swen225/gp22/app/Keys.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Keys.java
@@ -3,8 +3,21 @@ package nz.ac.vuw.ecs.swen225.gp22.app;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+/**
+ * Reduces the code needed for overrides.
+ */
 public interface Keys extends KeyListener {
+  /**
+   * Key typed keylistener
+   */
   default public void keyTyped(KeyEvent e){}
+   /**
+   * Key pressed keylistener
+   */
   void keyPressed(KeyEvent e);
+   /**
+   * Key released keylistener
+   */
   default public void keyReleased(KeyEvent e){}
 }
+
