68800cbf6bcdbe19ce88c76642f31d0a827eaa7b
grunfejack <grunfejack@ecs.vuw.ac.nz>
2022-10-13 19:43:21 +1300

    adding contuin loopn

--- src/sounds/sounds.java
+++ src/sounds/sounds.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.Currency;
 
 import javax.print.DocFlavor.URL;
 import javax.sound.sampled.AudioInputStream;
@@ -45,31 +46,14 @@ public class sounds {
                 clip.stop();
             }
         }
-    }
-
-
-
-
-    public Clip clip;
-    AudioInputStream sound;
-    public void setFile(String soundFileName) {
-        try {
-            File file = new File(soundFileName);
-            AudioInputStream sound = AudioSystem.getAudioInputStream(file);
-            clip = AudioSystem.getClip();
-            clip.open(sound);
-        } catch (Exception e) {
+        public void loop(){
+            Clip cur = clip;
+            cur.loop(Clip.LOOP_CONTINUOUSLY);
         }
     }
-    public void play() {
-        clip.start();
-    }
-    public void stop(){
-        clip.close();
-        clip.stop();
-    }
-
-    public void loop(){
-       // clip.loop(Clip.LOOP_CONTINUSESLY);
-    }
 }
+
+
+
+
+   
