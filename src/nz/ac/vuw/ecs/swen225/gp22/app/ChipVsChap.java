ebf7e7995d776a692e72837965825a9bf362906f
Calvin Li <callvinlinz@gmail.com>
2022-10-13 23:13:55 +1300

    game is supposed to close on save

--- src/nz/ac/vuw/ecs/swen225/gp22/app/ChipVsChap.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/ChipVsChap.java
@@ -560,7 +560,8 @@ public class ChipVsChap extends JFrame{
         });
         saveButton.addActionListener(e->{
             Persistency.createPXML(model.maze().getTiles(), model.maze().getChap().getInvKeys(),count);
-            menu();
+            dipose();
+            System.exit(ABORT);
         });
         quitButton.addActionListener(e->{
             replay = null;
