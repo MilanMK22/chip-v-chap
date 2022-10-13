c3862339edd7c7b1a37f72c26bb2629477d1d26d
Calvin Li <callvinlinz@gmail.com>
2022-10-13 21:33:15 +1300

    Javadoc comments being added into App component

--- src/nz/ac/vuw/ecs/swen225/gp22/app/Controller.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Controller.java
@@ -5,13 +5,22 @@ import java.util.Arrays;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
-
+/**
+ * Controller class that holds the keybindings relating to the movement of Chap.
+ * Contains methods used to alter the keybindings in ChipVsChap.
+ */
 public class Controller {
     public static Character[] characters = new Character[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     public static int[] arrows = {KeyEvent.VK_UP,KeyEvent.VK_DOWN,KeyEvent.VK_LEFT,KeyEvent.VK_RIGHT};
-    public static int[] controls = new int[]{KeyEvent.VK_UP,KeyEvent.VK_DOWN,KeyEvent.VK_LEFT,KeyEvent.VK_RIGHT};
     public static Character[] characterControls = new Character[]{	'\u2191', '\u2193','\u2190','\u2192'};
 
+    /**
+     * Used to translate KeyEvent variables to it's respective character variable.
+     * Only used when KeyEvent is an arrow key.
+     * 
+     * @param e KeyEvent that comes from KeyListener.
+     * @return e.getKeyChar() This is the converted KeyEvent into its arrow character.
+     */
     public static Character getArrow(KeyEvent e){
         switch(e.getKeyCode()){
             case KeyEvent.VK_UP:
@@ -25,6 +34,13 @@ public class Controller {
         }
        return e.getKeyChar();
     }  
+    /**
+     * This method will return the ExtendedKeyCode for the character argument.
+     * Only used to translate arrow key symbols to its KeyEvent variable.
+     * 
+     * @param c Arrow key letter.
+     * @return java.awt.event.KeyEvent.getExtendedKeyCodeForChar(c) This is the ExtendedKeyCode for character c.
+     */
     public static int getCode(Character c){
         switch(c){
             case '\u2191':
@@ -38,7 +54,13 @@ public class Controller {
         }
        return java.awt.event.KeyEvent.getExtendedKeyCodeForChar(c);
     }  
-
+    /**
+     * This method returns an actionlistener relative to the direction that the keybind relates to.
+     * 
+     * @param code Key code that correlates to the array index of the direction.
+     * @param component Button that correlates to the direction being changed.
+     * @return actionListener This is specific to direction.
+     */
     public static ActionListener reMap(int code, JButton component){
         return (e) -> {
                 component.setText("Waiting for input");
@@ -72,12 +94,11 @@ public class Controller {
                             component.setText(""+characterControls[code]);
                             component.removeKeyListener(this);
                             }
-                        updateKeys();
                     }
                 });
             };
         };
 
-    private static void updateKeys() {for (int i = 0; i < controls.length; i++) {controls[i] = java.awt.event.KeyEvent.getExtendedKeyCodeForChar(characterControls[i]);}}
+
 
 }
